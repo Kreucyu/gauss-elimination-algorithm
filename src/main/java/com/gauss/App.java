36dadd7aaa564020888b6e8aa01a105a7bc8dde3
Kreucyu <elciocleiton72@gmail.com>
2026-04-05 10:22:53 -0300

    build: projeto criado

---
+++ src/main/java/com/gauss/App.java
@@ -0,0 +1,13 @@
+package com.gauss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
