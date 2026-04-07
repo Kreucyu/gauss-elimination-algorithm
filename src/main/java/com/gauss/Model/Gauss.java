535b4473a8c555260bcaf3d3c338d141a68e643c
Kreucyu <elciocleiton72@gmail.com>
2026-04-07 15:26:35 -0300

    feat: realizar eliminação de Gauss e troca de linhas caso pivô seja 0

--- src/main/java/com/gauss/Model/Gauss.java
+++ src/main/java/com/gauss/Model/Gauss.java
@@ -12,10 +12,12 @@ public class Gauss {
 
         System.out.println("\nIniciando cálculo: ");
 
-        for(int i = 0; i < matriz.length - 1; i++) {
+        for(int i = 0; i < linhasMatriz - 1; i++) {
             double pivo = matriz[i][i];
             if(Math.abs(pivo) < 1e-9) {
-
+                substituirPivoNulo(matriz, i);
+                pivo = matriz[i][i];
+                System.out.println("\nPivô é 0, realizando troca com o valor " + pivo + "!");
             }
             System.out.printf("\nPivô atual: " + String.format("%.1f", pivo) + "\n");
             for(int j = i + 1; j < linhasMatriz; j++) {
@@ -28,8 +30,34 @@ public class Gauss {
             System.out.println("[A(" + (i + 1) + ")|b(" + (i + 1) + ")]:\n");
             leitor.exibirMatriz();
         }
+        definirValorVariaveis();
         linhasMatriz = 0;
         colunasMatriz = 0;
         leitor.limpar();
+    }
+
+    private void substituirPivoNulo(double[][] matriz, int i) {
+        double candidato = 0;
+        int linha = 0;
+        for(int p = i + 1; p < linhasMatriz; p++) {
+            if(Math.abs(matriz[p][i]) > 1e-9) {
+                if(matriz[p][i] > candidato) {
+                    candidato = matriz[p][i];
+                    linha = p;
+                }
+            }
+        }
+        if(candidato != 0 && linha != 0) {
+            double temp;
+            for(int j = 0; j < colunasMatriz; j++) {
+                temp = matriz[i][j];
+                matriz[i][j] = matriz[linha][j];
+                matriz[linha][j] = temp;
+            }
+        }
+    }
+
+    private void definirValorVariaveis() {
+        Classificador classificador = new Classificador();
     }
 }
