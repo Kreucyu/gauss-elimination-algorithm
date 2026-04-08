cb771492746f4db1fe05aed4edd7f7fe88484537
Kreucyu <elciocleiton72@gmail.com>
2026-04-08 10:27:52 -0300

    docs: documentação App.java

--- src/main/java/com/gauss/App.java
+++ src/main/java/com/gauss/App.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class App
 {
     public static void main( String[] args ) {
+        //iniciando sistema.
         Scanner sc = new Scanner(System.in);
         Menu menu = new Menu(sc);
         menu.exibirMenu();
